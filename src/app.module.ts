a02db7e83540bd24311209202392409f53effb64
MAG1980 <al-1980@inbox.ru>
2023-09-03 17:52:18 +0300

    В docker-compose.yml добавлен сервис Postgres. Начата разработка API для проекта Real World. Сгенерированы модуль и контроллер Tags.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { TopPageModule } from './top-page/top-page.module';
-import { ProductModule } from './product/product.module';
-import { ReviewModule } from './review/review.module';
-import { ReviewController } from './review/review.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypegooseModule } from 'nestjs-typegoose';
-import { getMongoConfig } from './configs/mongo.config';
-import { MoviesModule } from './movies/movies.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {AuthModule} from './auth/auth.module';
+import {TopPageModule} from './top-page/top-page.module';
+import {ProductModule} from './product/product.module';
+import {ReviewModule} from './review/review.module';
+import {ReviewController} from './review/review.controller';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {TypegooseModule} from 'nestjs-typegoose';
+import {getMongoConfig} from './configs/mongo.config';
+import {MoviesModule} from './movies/movies.module';
+import {TagsModule} from './tags/tags.module';
 
 @Module({
   imports: [
@@ -24,8 +25,10 @@ import { MoviesModule } from './movies/movies.module';
     ProductModule,
     ReviewModule,
     MoviesModule,
+    TagsModule,
   ],
   controllers: [AppController, ReviewController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/tags/tags.controller.ts
@@ -0,0 +1,12 @@
+import {Controller, Get} from '@nestjs/common';
+
+@Controller('tags')
+export class TagsController {
+@Get()
+  findAll(){
+  return [
+    "reactjs",
+    "angularjs"
+  ]
+}
+}
