a498c86c30b8ad3f3b3b761be9d2390e8779bf3d
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-09-11 16:20:38 +0300

    Созданы скрипты для генерации, создания, запуска и отмены миграций. Создан файл с настройками БД, использующий переменные окружения и экспортирующий по умолчанию DataSource.

--- src/configs/postgres.config.ts
+++ src/configs/postgres.config.ts
@@ -1,26 +1,20 @@
 import { config } from 'dotenv';
-import { ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import * as process from 'process';
 
 config();
 
-const configService = new ConfigService();
-
 const configOptions: PostgresConnectionOptions = {
-  type: configService.getOrThrow('DB_TYPE'),
-  host: configService.getOrThrow('DB_HOST'),
-  port: configService.getOrThrow('DB_PORT'),
-  database: configService.getOrThrow('POSTGRES_DB_NAME'),
-  username: configService.getOrThrow('POSTGRES_DB_USERNAME'),
-  password: configService.getOrThrow('POSTGRES_DB_PASSWORD'),
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.POSTGRES_DB_NAME,
+  username: process.env.POSTGRES_DB_USERNAME,
+  password: process.env.POSTGRES_DB_PASSWORD,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
 };
-
-export const getPostgresConfig = async (
-  configService: ConfigService,
-): Promise<PostgresConnectionOptions> => configOptions;
 
 export default new DataSource(configOptions);
